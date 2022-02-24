c0b15445e6acdf8d0d682da3d306014e0002c07b
aliff azfar <aliffazfar34@gmail.com>
2022-02-25 02:54:35 +0800

    styled-comp

---
+++ src/pages/Landing.js
@@ -0,0 +1,14 @@
+import { Logo } from '../components'
+import Wrapper from '../assets/wrappers/LandingPage'
+
+const Landing = () => {
+  return (
+    <Wrapper>
+      <nav>
+        <Logo />
+      </nav>
+    </Wrapper>
+  )
+}
+
+export default Landing
