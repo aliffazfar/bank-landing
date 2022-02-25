fd33dae0970b59f138ed896b8952fd16a9299ad5
aliff azfar <aliffazfar34@gmail.com>
2022-02-25 14:20:44 +0800

    nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
   NavbarContainer,
   LeftContainer,
@@ -7,23 +7,45 @@ import {
   NavbarExtendedContainer,
   NavbarInnerContainer,
   NavbarLink,
+  Logo,
+  OpenLinksButton,
+  NavbarLinkExtended,
 } from '../styles/Navbar.styles'
+import LogoImg from '../assets/images/logo.png'
 
 const Navbar = () => {
+  const [extendNavbar, setExtendNavbar] = useState(false)
+
   return (
-    <NavbarContainer>
+    <NavbarContainer extendNavbar={extendNavbar}>
       <NavbarInnerContainer>
-        <LeftContainer></LeftContainer>
+        <LeftContainer>
+          <Logo src={LogoImg}></Logo>
+        </LeftContainer>
         <RightContainer>
           <NavbarLinkContainer>
             <NavbarLink to='/about'> about</NavbarLink>
             <NavbarLink to='/portfolio'> portfolio</NavbarLink>
             <NavbarLink to='/resume'> resume</NavbarLink>
             <NavbarLink to='/contact'> contact</NavbarLink>
+            <OpenLinksButton
+              onClick={() => {
+                setExtendNavbar((curr) => !curr)
+              }}
+            >
+              {extendNavbar ? <>&#10005;</> : <> &#8801;</>}
+            </OpenLinksButton>
           </NavbarLinkContainer>
         </RightContainer>
       </NavbarInnerContainer>
-      <NavbarExtendedContainer></NavbarExtendedContainer>
+      {extendNavbar && (
+        <NavbarExtendedContainer>
+          <NavbarLinkExtended to='/about'> about</NavbarLinkExtended>
+          <NavbarLinkExtended to='/portfolio'> portfolio</NavbarLinkExtended>
+          <NavbarLinkExtended to='/resume'> resume</NavbarLinkExtended>
+          <NavbarLinkExtended to='/contact'> contact</NavbarLinkExtended>
+        </NavbarExtendedContainer>
+      )}
     </NavbarContainer>
   )
 }
--- src/styles/Navbar.styles.js
+++ src/styles/Navbar.styles.js
@@ -3,23 +3,27 @@ import { Link } from 'react-router-dom'
 
 export const NavbarContainer = styled.nav`
   width: 100%;
-  height: 80px;
+  height: ${(props) => (props.extendNavbar ? '100vh' : '80px')};
   background-color: black;
   display: flex;
   flex-direction: column;
+
+  @media (min-width: 700px) {
+    height: 80px;
+  }
 `
 export const LeftContainer = styled.div`
   flex: 30%;
   display: flex;
   align-items: center;
-  padding-left: 5%;
+  padding-left: 25%;
 `
 
 export const RightContainer = styled.div`
   flex: 70%;
   display: flex;
   align-items: center;
-  padding-left: 5%;
+  padding-left: 25%;
 `
 export const NavbarInnerContainer = styled.div`
   width: 100%;
@@ -37,6 +41,44 @@ export const NavbarLink = styled(Link)`
   font-family: Arial, Helvetica, sans-serif;
   text-decoration: none;
   margin: 10px;
+
+  @media (max-width: 700px) {
+    display: none;
+  }
 `
 
-export const NavbarExtendedContainer = styled.div``
+export const NavbarLinkExtended = styled(Link)`
+  color: white;
+  font-size: large;
+  font-family: Arial, Helvetica, sans-serif;
+  text-decoration: none;
+  margin: 10px;
+`
+export const Logo = styled.img`
+  margin: 10px;
+  max-width: 180px;
+  height: auto;
+`
+
+export const OpenLinksButton = styled.button`
+  width: 70px;
+  height: 50px;
+  background: none;
+  border: none;
+  color: white;
+  font-size: 45px;
+  cursor: pointer;
+  @media (min-width: 700px) {
+    display: none;
+  }
+`
+
+export const NavbarExtendedContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  @media (min-width: 700px) {
+    display: none;
+  }
+`
