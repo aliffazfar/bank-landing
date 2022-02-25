a977c8d4d7b590ae3759e2f6a7426e3282ed5236
aliff azfar <aliffazfar34@gmail.com>
2022-02-25 22:40:32 +0800

    compare slider

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -3,21 +3,35 @@ import {
   ReactCompareSliderImage,
 } from 'react-compare-slider'
 
+import { LandingContainer } from '../styles/Landing.styles'
+
 import img1 from '../assets/images/1.jpg'
 import img2 from '../assets/images/7.jpg'
 
 const Landing = () => {
   return (
-    <div>
-      <ReactCompareSlider
-        itemOne={
-          <ReactCompareSliderImage src={img1} srcSet={img1} alt='Image one' />
-        }
-        itemTwo={
-          <ReactCompareSliderImage src={img2} srcSet={img2} alt='Image two' />
-        }
-      />
-    </div>
+    <ReactCompareSlider
+      itemOne={
+        <LandingContainer className='img'>
+          <ReactCompareSliderImage
+            src={img1}
+            srcSet={img1}
+            className='img'
+            alt='Image one'
+          />
+        </LandingContainer>
+      }
+      itemTwo={
+        <LandingContainer className='img'>
+          <ReactCompareSliderImage
+            src={img2}
+            className='img'
+            srcSet={img2}
+            alt='Image two'
+          />
+        </LandingContainer>
+      }
+    />
   )
 }
 
---
+++ src/styles/Landing.styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const LandingContainer = styled.main`
+  height: 35rem;
+  margin-top: 2rem;
+  display: flex;
+  align-items: center;
+  justify-content: end;
+  flex-wrap: wrap;
+  gap: 1rem;
+
+  img {
+    height: 540px;
+  }
+`
